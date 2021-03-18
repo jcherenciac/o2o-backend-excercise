129d318b68276f163bf264635c170c44aaf7f444
Juan Carlos Herencia <jcherenciac@gmail.com>
2021-03-18 22:02:54 +0100

    Added searchBeerViewData service.

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -38,4 +38,17 @@ class ApiController extends AbstractController
         $arrayFieldsKeys = ['id', 'name', 'description'];
         return $this->beerService->searchBeer($params, $arrayFieldsKeys);
     }
+
+    /**
+     * @Route("beer/view", name="view_beer", methods={"GET"} )
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function searchBeerViewData(Request $request): JsonResponse
+    {
+        $params = $request->query->all();
+        $arrayFieldsKeys = ['id', 'name', 'description', 'image_url', 'tagline', 'first_brewed
+        '];
+        return $this->beerService->searchBeer($params, $arrayFieldsKeys);
+    }
 }
